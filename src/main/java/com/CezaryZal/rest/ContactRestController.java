934d481200b1180bed3393442300166614762e0d
CezaryZal <czarek0821@gmail.com>
2019-09-02 12:22:17 +0200

    add CRUD method

--- src/main/java/com/CezaryZal/rest/ContactRestController.java
+++ src/main/java/com/CezaryZal/rest/ContactRestController.java
@@ -4,10 +4,7 @@ import com.CezaryZal.entity.Contact;
 import com.CezaryZal.rest.Exception.ContactNotFoundException;
 import com.CezaryZal.service.ContactService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,14 +20,41 @@ public class ContactRestController {
         return contactService.getContacts();
     }
 
-    @GetMapping("/contacts/{contactId}")
+    @GetMapping("/contact/{contactId}")
     public Contact getContact (@PathVariable int contactId){
         Contact contact = contactService.getContact(contactId);
         if (contact == null){
             throw new ContactNotFoundException("Contact id not found - " + contactId);
         }
+        return contact;
+    }
+
+    @PostMapping("contact")
+    public Contact addContact (@RequestBody Contact contact){
+        contact.setId(0);
+        contactService.saveContact(contact);
 
         return contact;
     }
+
+    @PutMapping("/contact")
+    public Contact updateContact (@RequestBody Contact contact){
+        contactService.saveContact(contact);
+
+        return contact;
+    }
+
+    @DeleteMapping("/contact/{contactId}")
+    public String deleteContact(@PathVariable int contactId){
+        Contact contact = contactService.getContact(contactId);
+        if (contact == null){
+            throw new ContactNotFoundException("Contact id not found - " + contactId);
+        }
+        contactService.deleteContact(contactId);
+
+        return "Delete contact id - " + contactId;
+    }
+
+
 
 }
